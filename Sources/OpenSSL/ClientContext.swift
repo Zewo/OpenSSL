dcc3f8744b722f738d0077006c2ad7c97c9ed25f
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-10 00:00:15 +0900

    Simple fix for now
    Just removed Cipher List Specification otherwise we find clean solution.

--- Sources/OpenSSL/ClientContext.swift
+++ Sources/OpenSSL/ClientContext.swift
@@ -30,22 +30,12 @@ public final class SSLClientContext: Context {
     public init(verifyBundle: String? = nil,
       certificate: String? = nil,
       privateKey: String? = nil,
-      certificateChain: String? = nil,
-      cipherList: String? = nil) throws {
+      certificateChain: String? = nil) throws {
 		try super.init(method: .SSLv23, type: .Client)
 
         SSL_CTX_set_verify(context, SSL_VERIFY_PEER, nil)
         SSL_CTX_set_verify_depth(context, 4)
         SSL_CTX_set_options(context, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION)
-
-        var sslCipherList = "HIGH:!aNULL:!kRSA:!PSK:!SRP:!MD5:!RC4"
-        if let cipherList = cipherList {
-          sslCipherList = cipherList
-        }
-        if SSL_CTX_set_cipher_list(context, sslCipherList) != 1 {
-            throw Context.Error.Certificate(description: lastSSLErrorDescription)
-        }
-
 
         if SSL_CTX_set_default_verify_paths(context) != 1 {
             throw Context.Error.Certificate(description: lastSSLErrorDescription)
